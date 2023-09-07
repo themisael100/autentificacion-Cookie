e3ee2f4b40a8688fc456d79e8069439d8da492ee
Bryan Misael Sanchez Ramirez <misalbryan@gmail.com>
2023-09-07 11:44:06 -0600

    Finalizacion del Ejercicio

--- BMSR20230907RegistroAcademico/BMSR20230907RegistroAcademico/Controllers/MatrículaController.cs
+++ BMSR20230907RegistroAcademico/BMSR20230907RegistroAcademico/Controllers/MatrículaController.cs
@@ -1,13 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+﻿//Importa el espacio de nombres necesarios para la autorizacion
+using Microsoft.AspNetCore.Authorization;
+//Importa el espacio de nombres necesarios para trabajar con controladores
+using Microsoft.AspNetCore.Mvc;
+//Importa el espacio de nombres necesarios para trabajar con modelos
+using BMSR20230907RegistroAcademico.Models;
 
 namespace BMSR20230907RegistroAcademico.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    //Aplica la autorizacion a las acciones, lo que significa que solo los usuarios autenticados pueden acceder a ellas 
+    [Authorize]
     public class MatrículaController : ControllerBase
     {
+        //Declaracion de una lista estatica de objetos 'Matriculas' para almacanea matriculas registradas
+        static List<Matriculas> matricula = new List<Matriculas>();
 
+        [HttpPost("CrearMatricula")]
+        public IActionResult Post([FromBody] Matriculas matriculas)
+        {
+            matricula.Add(matriculas);
+            return Ok();
+        }
+
+
+
+        //Definicion de un metodo HTTP PUT para actualizar un cliente existente en la lista por su ID
+        [HttpPut("{id}/ModificarMatricula")]
+
+        public IActionResult Put(int id, [FromBody] Matriculas matriculas)
+        {
+            var existingMatricula = matricula.FirstOrDefault(M => M.Id == id);
+            if (existingMatricula != null)
+            {
+
+                existingMatricula.NameStudent = matriculas.NameStudent;
+                existingMatricula.Classroom = matriculas.Classroom;
+          
+                return Ok();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+
+        //Definicion de un metodo HTTP GET que recibe un ID como parametro y devuelve una Matricula especifica
+        [HttpGet("{id}/ObtenerPorIdMatricula")]
+        public Matriculas Get(int id)
+        {
+            var matriculas = matricula.FirstOrDefault(M => M.Id == id);
+            return matriculas;
+        }
     }
 }
---
+++ BMSR20230907RegistroAcademico/BMSR20230907RegistroAcademico/Models/Matriculas.cs
@@ -0,0 +1,9 @@
+﻿namespace BMSR20230907RegistroAcademico.Models
+{
+    public class Matriculas
+    {
+        public int Id { get; set; }
+        public string NameStudent { get; set; }
+        public string Classroom { get;  set; }
+    }
+}
